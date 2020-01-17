8568eee6eb7d96ee5b39d07338e8352145b8d3b4
ejfisher <49263228+ejfisher@users.noreply.github.com>
2020-01-17 16:24:37 -0700

    Add files via upload

--- csuGPS.py
+++ csuGPS.py
@@ -30,21 +30,13 @@ def acquire():
 	# and other details.
 	global gps
 	global uart
-
-
-
-
 	# Main loop runs forever printing the location, etc. every second.
-	last_print = time.monotonic()
 	# Make sure to call gps.update() every loop iteration and at least twice
 	# as fast as data comes from the GPS unit (usually every second).
 	# This returns a bool that's true if it parsed new data (you can ignore it
 	# though if you don't care and instead look at the has_fix property).
 	gps.update()
 	# Every second print out current location details if there's a fix.
-	current = time.monotonic()
-	if current - last_print >= 1.0:
-		last_print = current
 	if not gps.has_fix:
 		# Try again if we don't have a fix yet.
 		print('Waiting for fix...')
@@ -52,13 +44,6 @@ def acquire():
 	# We have a fix! (gps.has_fix is true)
 	# Print out details about the fix like location, date, etc.
 	print('=' * 40)  # Print a separator line.
-	print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
-	gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
-	gps.timestamp_utc.tm_mday,  # struct_time object that holds
-	gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-	gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-	gps.timestamp_utc.tm_min,   # month!
-	gps.timestamp_utc.tm_sec))
 	print('Latitude: {0:.6f} degrees'.format(gps.latitude))
 	print('Longitude: {0:.6f} degrees'.format(gps.longitude))
 	print('Fix quality: {}'.format(gps.fix_quality))
