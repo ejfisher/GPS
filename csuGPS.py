8d9fdf3e85b58baf07991b2743baa00d2f350c28
ejfisher <49263228+ejfisher@users.noreply.github.com>
2020-01-17 15:41:51 -0700

    tabs

--- csuGPS.py
+++ csuGPS.py
@@ -23,33 +23,33 @@ def acquire():
 
 	gps.update()
 	if not gps.has_fix:
-        # Try again if we don't have a fix yet.
-        print('Waiting for fix...')
-        continue
-    # We have a fix! (gps.has_fix is true)
-    # Print out details about the fix like location, date, etc.
-    print('=' * 40)  # Print a separator line.
-    print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
-        gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
-        gps.timestamp_utc.tm_mday,  # struct_time object that holds
-        gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-        gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-        gps.timestamp_utc.tm_min,   # month!
-        gps.timestamp_utc.tm_sec))
-    print('Latitude: {0:.6f} degrees'.format(gps.latitude))
-    print('Longitude: {0:.6f} degrees'.format(gps.longitude))
-    print('Fix quality: {}'.format(gps.fix_quality))
-    # Some attributes beyond latitude, longitude and timestamp are optional
-    # and might not be present.  Check if they're None before trying to use!
-    if gps.satellites is not None:
-        print('# satellites: {}'.format(gps.satellites))
-    if gps.altitude_m is not None:
-        print('Altitude: {} meters'.format(gps.altitude_m))
-    if gps.speed_knots is not None:
-        print('Speed: {} knots'.format(gps.speed_knots))
-    if gps.track_angle_deg is not None:
-        print('Track angle: {} degrees'.format(gps.track_angle_deg))
-    if gps.horizontal_dilution is not None:
-        print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
-    if gps.height_geoid is not None:
-        print('Height geo ID: {} meters'.format(gps.height_geoid))
+	# Try again if we don't have a fix yet.
+		print('Waiting for fix...')
+		continue
+	# We have a fix! (gps.has_fix is true)
+	# Print out details about the fix like location, date, etc.
+	print('=' * 40)  # Print a separator line.
+	print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
+	gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
+	gps.timestamp_utc.tm_mday,  # struct_time object that holds
+	gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+	gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+	gps.timestamp_utc.tm_min,   # month!
+	gps.timestamp_utc.tm_sec))
+	print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+	print('Longitude: {0:.6f} degrees'.format(gps.longitude))
+	print('Fix quality: {}'.format(gps.fix_quality))
+	# Some attributes beyond latitude, longitude and timestamp are optional
+	# and might not be present.  Check if they're None before trying to use!
+	if gps.satellites is not None:
+		print('# satellites: {}'.format(gps.satellites))
+	if gps.altitude_m is not None:
+		print('Altitude: {} meters'.format(gps.altitude_m))
+	if gps.speed_knots is not None:
+		print('Speed: {} knots'.format(gps.speed_knots))
+	if gps.track_angle_deg is not None:
+		print('Track angle: {} degrees'.format(gps.track_angle_deg))
+	if gps.horizontal_dilution is not None:
+		print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
+	if gps.height_geoid is not None:
+		print('Height geo ID: {} meters'.format(gps.height_geoid))
