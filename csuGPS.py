fdb78ef41656cbf1ba6b2d56ddfff43648570bb9
ejfisher <49263228+ejfisher@users.noreply.github.com>
2020-01-17 16:52:22 -0700

    Add files via upload

--- csuGPS.py
+++ csuGPS.py
@@ -47,22 +47,26 @@ def acquire():
 	time = (gps.timestamp_utc.tm_hour, gps.timestamp_utc.tm_min, gps.timestamp_utc.tm_sec)
 	gpsData = (gps.latitude, gps.longitude, gps.altitude_m, gps.speed_knots, gps.track_angle_deg, gps.horizontal_dilution)
 	gpsQuality = (gps.fix_quality, gps.satellites)
-	print('=' * 40)  # Print a separator line.
-	print('Fix timestamp: {0[0]:02}:{0[1]:02}:{0[2]:02} '.format(time))
-	print('Latitude: {0:.6f} degrees'.format(gps.latitude))
-	print('Longitude: {0:.6f} degrees'.format(gps.longitude))
-	print('Fix quality: {}'.format(gps.fix_quality))
+	
+	#section that prints out Data
+	# print('=' * 40)  # Print a separator line.
+	# print('Fix timestamp: {0[0]:02}:{0[1]:02}:{0[2]:02} '.format(time))
+	# print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+	# print('Longitude: {0:.6f} degrees'.format(gps.longitude))
+	# print('Fix quality: {}'.format(gps.fix_quality))
 	# Some attributes beyond latitude, longitude and timestamp are optional
 	# and might not be present.  Check if they're None before trying to use!
-	if gps.satellites is not None:
-		print('# satellites: {}'.format(gps.satellites))
-	if gps.altitude_m is not None:
-		print('Altitude: {} meters'.format(gps.altitude_m))
-	if gps.speed_knots is not None:
-		print('Speed: {} knots'.format(gps.speed_knots))
-	if gps.track_angle_deg is not None:
-		print('Track angle: {} degrees'.format(gps.track_angle_deg))
-	if gps.horizontal_dilution is not None:
-		print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
-	if gps.height_geoid is not None:
-		print('Height geo ID: {} meters'.format(gps.height_geoid))
+	# if gps.satellites is not None:
+	# 	print('# satellites: {}'.format(gps.satellites))
+	# if gps.altitude_m is not None:
+	# 	print('Altitude: {} meters'.format(gps.altitude_m))
+	# if gps.speed_knots is not None:
+	# 	print('Speed: {} knots'.format(gps.speed_knots))
+	# if gps.track_angle_deg is not None:
+	# 	print('Track angle: {} degrees'.format(gps.track_angle_deg))
+	# if gps.horizontal_dilution is not None:
+	# 	print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
+	# if gps.height_geoid is not None:
+	# 	print('Height geo ID: {} meters'.format(gps.height_geoid))
+
+	return time, gpsData, gpsQuality
--- csuIF.py
+++ csuIF.py
@@ -5,4 +5,8 @@ import adafruit_gps
 #print(type(gps))
 
 csuGPS.init()
-csuGPS.acquire()
+time, gpsData,gpsQuality = csuGPS.acquire()
+print('=' * 40)  # Print a separator line.
+print('Fix timestamp: {0[0]:02}:{0[1]:02}:{0[2]:02} '.format(time))
+print('Latitude: {0[0]:.6f} degrees\nLongitude: {0[1]:.6f} degrees'.format(gpsData))
+print('Fix quality: {0[0]}\n# satellites: {0[1]}'.format(gpsQuality))
