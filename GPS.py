15ad07a732a1c736d246be3fd62e8a34b2f85ae5
ejfisher <49263228+ejfisher@users.noreply.github.com>
2020-01-17 15:29:28 -0700

    DOUBLE THE SPEEED

--- GPS.py
+++ GPS.py
@@ -4,49 +4,25 @@
 import time
 import board
 import busio
+import serial
  
 import adafruit_gps
- 
-# Create a serial connection for the GPS connection using default speed and
-# a slightly higher timeout (GPS modules typically update once a second).
-# These are the defaults you should use for the GPS FeatherWing.
-# For other boards set RX = GPS module TX, and TX = GPS module RX pins.
-#uart = busio.UART(board.TXD, board.RXD, baudrate=9600, timeout=10)
- 
+
 # for a computer, use the pyserial library for uart access
-import serial
+
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=10)
- 
-# If using I2C, we'll create an I2C interface to talk to using default pins
-#i2c = busio.I2C(board.SCL, board.SDA)
  
 # Create a GPS module instance.
 gps = adafruit_gps.GPS(uart, debug=False)     # Use UART/pyserial
-#gps = adafruit_gps.GPS_GtopI2C(i2c, debug=False)  # Use I2C interface
- 
-# Initialize the GPS module by changing what data it sends and at what rate.
-# These are NMEA extensions for PMTK_314_SET_NMEA_OUTPUT and
-# PMTK_220_SET_NMEA_UPDATERATE but you can send anything from here to adjust
-# the GPS module behavior:
-#   https://cdn-shop.adafruit.com/datasheets/PMTK_A11.pdf
  
 # Turn on the basic GGA and RMC info (what you typically want)
 gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# Turn on just minimum info (RMC only, location):
-#gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# Turn off everything:
-#gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-# Tuen on everything (not all of it is parsed!)
-#gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
+
  
 # Set update rate to once a second (1hz) which is what you typically want.
-gps.send_command(b'PMTK220,1000')
-# Or decrease to once every two seconds by doubling the millisecond value.
-# Be sure to also increase your UART timeout above!
-#gps.send_command(b'PMTK220,2000')
-# You can also speed up the rate, but don't go too fast or else you can lose
-# data during parsing.  This would be twice a second (2hz, 500ms delay):
-#gps.send_command(b'PMTK220,500')
+# gps.send_command(b'PMTK220,1000')
+# This would be twice a second (2hz, 500ms delay):
+gps.send_command(b'PMTK220,500')
  
 # Main loop runs forever printing the location, etc. every second.
 last_print = time.monotonic()
@@ -58,7 +34,7 @@ while True:
     gps.update()
     # Every second print out current location details if there's a fix.
     current = time.monotonic()
-    if current - last_print >= 1.0:
+    if current - last_print >= 0.5:
         last_print = current
         if not gps.has_fix:
             # Try again if we don't have a fix yet.
