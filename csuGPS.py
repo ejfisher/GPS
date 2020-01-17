d461e698a30b7bc2c80ffd604752116df71d41d2
ejfisher <49263228+ejfisher@users.noreply.github.com>
2020-01-17 15:53:35 -0700

    Add files via upload

--- csuGPS.py
+++ csuGPS.py
@@ -32,13 +32,7 @@ def acquire(gps):
 	# We have a fix! (gps.has_fix is true)
 	# Print out details about the fix like location, date, etc.
 	print('=' * 40)  # Print a separator line.
-	print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
-	gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
-	gps.timestamp_utc.tm_mday,  # struct_time object that holds
-	gps.timestamp_utc.tm_year,  # the fix time.  Note you might
-	gps.timestamp_utc.tm_hour,  # not get all data like year, day,
-	gps.timestamp_utc.tm_min,   # month!
-	gps.timestamp_utc.tm_sec))
+	print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(gps.timestamp_utc.tm_mon, gps.timestamp_utc.tm_mday, gps.timestamp_utc.tm_year, gps.timestamp_utc.tm_hour, gps.timestamp_utc.tm_min, gps.timestamp_utc.tm_sec))
 	print('Latitude: {0:.6f} degrees'.format(gps.latitude))
 	print('Longitude: {0:.6f} degrees'.format(gps.longitude))
 	print('Fix quality: {}'.format(gps.fix_quality))
