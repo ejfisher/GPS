ecd130d01163f6e75cb970c091714958b913b670
ejfisher <49263228+ejfisher@users.noreply.github.com>
2020-01-17 16:57:38 -0700

    Finalized GPS file to return Tuples as needed

--- csuIF.py
+++ csuIF.py
@@ -12,5 +12,5 @@ csuGPS.init()
 time, gpsData,gpsQuality = csuGPS.acquire()
 print('=' * 40)  # Print a separator line.
 print('Fix timestamp: {0[0]:02}:{0[1]:02}:{0[2]:02} '.format(time))
-print('Latitude: {0[0]:.6f} degrees\nLongitude: {0[1]:.6f} degrees\nAltitude: {0[2]} meters'.format(gps.altitude_m))'.format(gpsData))
+print('Latitude: {0[0]:.6f} degrees\nLongitude: {0[1]:.6f} degrees\nAltitude: {0[2]} meters'.format(gpsData))
 print('Fix quality: {0[0]}\n# satellites: {0[1]}'.format(gpsQuality))
