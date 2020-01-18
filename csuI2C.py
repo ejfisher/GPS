252e401ec660e6dab078194253f83745bb8df20a
ejfisher <49263228+ejfisher@users.noreply.github.com>
2020-01-18 13:14:49 -0700

    Add files via upload

--- csuI2C.py
+++ csuI2C.py
@@ -9,6 +9,7 @@ def init():
 	global fxas
 
 	i2c = busio.I2C(board.SCL, board.SDA)
+	print(busio.scan())
 	fxos = adafruit_fxos8700.FXOS8700(i2c)
 	fxas = adafruit_fxas21002c.FXAS21002C(i2c)
 
@@ -18,4 +19,3 @@ def acquire():
 	print('Magnetometer (uTesla): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(*fxos.magnetometer))
 	print('Gyroscope (radians/s): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(*fxas.gyroscope))
 	return fxos.accelerometer, fxos.magnetometer, fxas.gyroscope
-	
--- csuIF.py
+++ csuIF.py
@@ -10,6 +10,14 @@ csuI2C.init()
 # gpsData = (Latitude, Longitude, Altitude, Speed, TAD, HD)
 # gpsQuality = (fix-quality, # of Sattelites)
 time, gpsData,gpsQuality = csuGPS.acquire()
+acc, mag, gyro = csuI2C.acquire()
+
+
+
+#display data aquired
+print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(acc))
+print('Magnetometer (uTesla): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag))
+print('Gyroscope (radians/s): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro))
 print('=' * 40)  # Print a separator line.
 print('Fix timestamp: {0[0]:02}:{0[1]:02}:{0[2]:02} '.format(time))
 print('Latitude: {0[0]:.6f} degrees\nLongitude: {0[1]:.6f} degrees\nAltitude: {0[2]} meters'.format(gpsData))
