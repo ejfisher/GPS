4bc029f6ca9c6fc441509f4b0df3254a9da460e2
ejfisher <49263228+ejfisher@users.noreply.github.com>
2020-01-18 18:24:44 -0700

    Add files via upload

--- csuDM.py
+++ csuDM.py
@@ -1,10 +1,10 @@
 def init(filename):
-	f = open(filename, 'w')
+	f = open("Data/" + filename, 'w')
 	f.write(filename[:-4] + "\n")
 	f.close()
 
 def write(filename, dArray):
-	f = open(filename, 'a')
+	f = open("Data/" + filename, 'a')
 	for val in dArray:
 		f.write(str(val) + ', ')
 	f.write('\n')
