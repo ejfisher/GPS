aa4b09a7a082b9d9e0ffc40cab61d3216b6d5260
ejfisher <49263228+ejfisher@users.noreply.github.com>
2020-01-18 18:37:55 -0700

    Add files via upload

--- csuIF.py
+++ csuIF.py
@@ -22,22 +22,20 @@ for i in range(30):
 	#Take the aquired data and output to a file
 	for i in range(7):
 		csuDM.write(dNames[i], bigData[i])
-		print("writing to the file")
+		#print("writing to the file")
 
 	#display data aquired
-	print('=' * 40)  # Print a separator line.
+	print('=' * 60)  # Print a separator line.
+	print('Time: {0[0]:02}:{0[1]:02}:{0[2]:02} '.format(gpsTime))
+	print('=' * 60)  # Print a separator line.
 	print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(*acc))
 	print('Magnetometer (uTesla): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(*mag))
 	print('Gyroscope (radians/s): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(*gyro))
-	print('=' * 40)  # Print a separator line.
-	print('Fix timestamp: {0[0]:02}:{0[1]:02}:{0[2]:02} '.format(gpsTime))
+	print('=' * 60)  # Print a separator line.
 	print('Latitude: {0[0]:.6f} degrees\nLongitude: {0[1]:.6f} degrees\nAltitude: {0[2]} meters'.format(gpsData))
 	print('Fix quality: {0[0]}\n# satellites: {0[1]}'.format(gpsQuality))
-	print('=' * 40)  # Print a separator line.
+	print('=' * 60)  # Print a separator line.
 	print('Pressure: {0[0]:0.3f} pascals\nAltitude: {0[1]:0.3f} meters\nTemperature: {0[2]:0.3f} degrees Celsius'.format(mpl))
 	time.sleep(1)
 
-
-
-
-print('=' * 40)  # Print a separator line.
+print('=' * 60)  # Print a separator line.
